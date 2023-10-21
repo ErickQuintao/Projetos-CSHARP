fe51ba924b29b8f445336312710a5bf3a78bab3e
Erick Quintao <erickquintao@outlook.com>
2023-10-21 19:12:28 -0300

    primeiro proj wev C#

---
+++ MeuSiteMVC/MeuSiteMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeuSiteMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
