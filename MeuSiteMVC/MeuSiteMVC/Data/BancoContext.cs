8088a1e550c57e780a9cb49a8fcdc4d6f21533e9
Erick Quintao <erickquintao@outlook.com>
2023-10-23 16:23:06 -0300

    half updrage

---
+++ MeuSiteMVC/MeuSiteMVC/Data/BancoContext.cs
@@ -0,0 +1,13 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Data
+{
+    public class BancoContext  : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options): base(options) 
+        {
+        }
+        public DbSet<ContatoModel> Contato { get; set; }
+    }
+}
---
+++ MeuSiteMVC/MeuSiteMVC/Models/ContatoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Emial { get; set;}
+        public string Celular { get; set;}
+    }
+}
