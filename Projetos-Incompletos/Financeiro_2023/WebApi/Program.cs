a126dabfb938182e4915ef2da60e7232e3ab7e4d
Erick Quintao <erickquintao@outlook.com>
2023-11-01 19:55:14 -0300

    json web token

--- Projetos-Incompletos/Financeiro_2023/WebApi/Program.cs
+++ Projetos-Incompletos/Financeiro_2023/WebApi/Program.cs
@@ -9,7 +9,10 @@ using Entities.Entidades;
 using Infra.Configuracao;
 using Infra.Repositorio;
 using Infra.Repositorio.Generics;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebApi.Token;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,6 +46,37 @@ builder.Services.AddSingleton<ISistemaFinanceiroServico, SistemaFinanceiroServic
 builder.Services.AddSingleton<IUsuarioSistemaFinanceiroServico, UsuarioSistemaFinanceiroServico>();
 
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+             .AddJwtBearer(option =>
+             {
+                 option.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateIssuer = false,
+                     ValidateAudience = false,
+                     ValidateLifetime = true,
+                     ValidateIssuerSigningKey = true,
+
+                     ValidIssuer = "Teste.Securiry.Bearer",
+                     ValidAudience = "Teste.Securiry.Bearer",
+                     IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+                 };
+
+                 option.Events = new JwtBearerEvents
+                 {
+                     OnAuthenticationFailed = context =>
+                     {
+                         Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                         return Task.CompletedTask;
+                     },
+                     OnTokenValidated = context =>
+                     {
+                         Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                         return Task.CompletedTask;
+                     }
+                 };
+             });
+
+
 
 var app = builder.Build();
 
@@ -54,6 +88,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ Projetos-Incompletos/Financeiro_2023/WebApi/Token/JwtSecurityKey.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace WebApi.Token
+{
+    public class JwtSecurityKey
+    {
+        public DateTime ValidTo { get; internal set; }
+
+        public static SymmetricSecurityKey Create(string secret)
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret));
+        }
+
+        public static implicit operator JwtSecurityKey(JwtSecurityToken v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator JwtSecurityKey(SecurityKey v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
