228f34c8e316fbd10641a6c5be44db96ef3b41e6
Erick Quintao <erickquintao@outlook.com>
2023-11-04 16:47:14 -0300

    rescontruir o projeto e finamente conseguir conectar db com c#

---
+++ Projetos-Incompletos/JWT-SistemaFinanceiro-2023/Domain/Interfaces/InterfaceServicos/IDespesaServico.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.InterfaceServicos
+{
+    public interface IDespesaServico
+    {
+        Task AdicionarDespesa(Despesa despesa);
+        Task AtualizarDespesa(Despesa despesa);
+    }
+}
---
+++ Projetos-Incompletos/JWT-SistemaFinanceiro-2023/Domain/Servicos/DespesaServico.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.InterfaceServicos;
+using Entities.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Servicos
+{
+    public class DespesaServico : IDespesaServico
+    {
+
+        private readonly InterfaceDespesa _InterfaceDespesa;
+        public DespesaServico(InterfaceDespesa InterfaceDespesa)
+        {
+            _InterfaceDespesa = InterfaceDespesa;
+        }
+
+        public async Task AdicionarDespesa(Despesa despesa)
+        {
+            var data = DateTime.UtcNow;
+            despesa.DataCadastro = data;
+            despesa.Ano = data.Year;
+            despesa.Mes = data.Month;
+
+            var valido = despesa.ValidarPropriedadeString(despesa.Nome, "Nome");
+            if (valido)
+                await _InterfaceDespesa.Add(despesa);
+
+        }
+
+        public async Task AtualizarDespesa(Despesa despesa)
+        {
+            var data = DateTime.UtcNow;
+            despesa.DataAlteracao = data;
+
+            if (despesa.Pago)
+            {
+                despesa.DataPagamento = data;
+            }
+
+            var valido = despesa.ValidarPropriedadeString(despesa.Nome, "Nome");
+            if (valido)
+                await _InterfaceDespesa.Update(despesa);
+        }
+    }
+}
