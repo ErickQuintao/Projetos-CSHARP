cc4ee45c2ffc0d4991f92923bc376dc097c96b0a
Erick Quintao <erickquintao@outlook.com>
2023-11-05 13:38:24 -0300

    project add

--- Projetos-Incompletos/JWT-SistemaFinanceiro-2023/WebApi/Models/Login.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApi.Models
-{
-    public class Login
-    {
-        public string email { get; set; }
-        public string senha { get; set; }
-        public string cpf { get; set; }
-    }
-}
