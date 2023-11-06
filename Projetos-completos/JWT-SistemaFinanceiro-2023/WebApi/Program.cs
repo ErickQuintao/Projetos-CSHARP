8aaca59afdf90d9e60b875202d1c76426143bfcd
Erick Quintao <erickquintao@outlook.com>
2023-11-06 16:04:12 -0300

    add o cors

--- Projetos-completos/JWT-SistemaFinanceiro-2023/WebApi/Program.cs
+++ Projetos-completos/JWT-SistemaFinanceiro-2023/WebApi/Program.cs
@@ -87,6 +87,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+var devClient = "http://localhost:4200";
+app.UseCors(x => 
+x.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader()
+.WithOrigins(devClient));
+
 app.UseHttpsRedirection();
 
 //New
